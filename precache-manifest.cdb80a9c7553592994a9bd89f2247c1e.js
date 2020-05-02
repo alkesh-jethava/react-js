d72e49693f6f05160f92267b8484850ea62360d8
Alkesh Jethava <alkeshjethava@gmail.com>
2020-05-02 16:18:19 +0530

    Updates

---
+++ precache-manifest.cdb80a9c7553592994a9bd89f2247c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c973c312cf46f91aaabfc2ee41bfe41a",
+    "url": "/react-js/index.html"
+  },
+  {
+    "revision": "f4ba70f6fe13d2633c3e",
+    "url": "/react-js/static/css/main.754a16db.chunk.css"
+  },
+  {
+    "revision": "d18caaab1ee52989c7c2",
+    "url": "/react-js/static/js/2.b322497a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-js/static/js/2.b322497a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ba70f6fe13d2633c3e",
+    "url": "/react-js/static/js/main.568406ba.chunk.js"
+  },
+  {
+    "revision": "3b8c710d4a076929cacf",
+    "url": "/react-js/static/js/runtime-main.a880fbbe.js"
+  }
+]);
